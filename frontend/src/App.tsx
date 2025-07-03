fc628026e984e917bf39f4f650775b2fcff3d0cd
Luke Zhuang <lukezhuang@Lukes-Laptop-7.local>
2025-07-02 18:02:21 -0700

    successfully added shadcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
      <h1 className="text-red-500 text-5xl">Hello</h1>
+     <Button variant={"outline"}>This is a button</Button>
     </>
   )
 }
