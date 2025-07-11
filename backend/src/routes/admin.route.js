1ac050aa5993ffc043944080cd860dd5c55432c2
Luke Zhuang <lukezhuang@Lukes-Laptop-7.attlocal.net>
2025-07-10 18:25:09 -0700

    set up routes using express :)

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -3,8 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-    console.log("✅ GET /api/users route hit");
-    res.send("User route with GET method");
+    res.send("Admin route with GET method");
 });
 
 export default router;
