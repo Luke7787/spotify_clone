3ed0457fdecc97f7acf768a5147109ac2fe54194
Luke Zhuang <lukezhuang@Lukes-Laptop-7.attlocal.net>
2025-07-24 20:00:58 -0700

    finished implementing auth check for new and existing users

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,9 @@
 import { Router } from "express";
+import { User } from "../models/user.model.js";
+import { authCallback } from "../controller/auth.controller.js";
 
 const router = Router();
 
-router.post("/callback", async (req, res) => {
-    try{
-        const {id, firstName, lastName, imageUrl} = req.body;
-
-        // check if user already exists
-    } catch (error){
-
-    }
-});
+router.post("/callback", authCallback);
 
 export default router;
