6b70a3cba816964f2e963b1cb42c37e9aa3c4052
Luke Zhuang <lukezhuang@Lukes-Laptop-7.attlocal.net>
2025-09-02 17:22:58 -0700

    worked on user routes & controller, along with starting on stats route

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import { Song } from "../models/song.model.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Stat route with GET method");
+router.get("/", async (req, res, next) => {
+
+    try {
+
+      const totalSongs = await Song
+
+    } catch (error) {
+        
+    }
 });
 
 export default router;
