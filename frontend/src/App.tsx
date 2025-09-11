cdf5450ee86c9ecd99b597dacff31d8649484137
Luke Zhuang <lukezhuang@Lukes-Laptop-7.attlocal.net>
2025-09-11 16:58:21 -0700

    updated local sever port to 5000, adding browerRoutering in main.tsx and installed react-router-dom for that

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,12 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
-import { Button } from "./components/ui/button"
+import { Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-     <header>
-      <SignedOut>
-        <SignInButton> 
-          <Button>
-            Sign In
-          </Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+     <Routes>
+
+     </Routes>
     </>
   )
 }
