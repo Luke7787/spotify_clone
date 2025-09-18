e3ff18faa5e15c72314dff441db84a0518ac2f45
Luke Zhuang <lukezhuang@Lukes-Laptop-7.attlocal.net>
2025-09-17 20:39:23 -0700

    started implementing authProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage";
-import { axiosInstance } from "./lib/axios";
 
 function App() {
-  // token
-  const getSomeData = async () => {
-    await axiosInstance.get("http://localhost:5000/api");
-  };
-
   return (
     <>
       <Routes>
