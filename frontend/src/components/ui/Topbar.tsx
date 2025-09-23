60f6aa80498381975edbeaf6aaff488277c57cb9
Luke Zhuang <lukezhuang@Lukes-Laptop-7.attlocal.net>
2025-09-22 18:13:38 -0700

    add top bar and sign-in button

--- frontend/src/components/ui/Topbar.tsx
+++
@@ -1,9 +0,0 @@
-const TopBar = () => {
-  return (
-    <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75">
-      TopBar
-    </div>
-  );
-};
-
-export default TopBar;
